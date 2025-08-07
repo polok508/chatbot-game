9c8c664324328c3e100fe9879769f08818b5b74d
polok508 <polok508@gmail.com>
2025-08-07 20:17:40 +0300

    Адаптивка под все браузеры

--- config.js
+++ config.js
@@ -9,8 +9,19 @@ const config = {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
     orientation: Phaser.Scale.LANDSCAPE,
-
   }
 };
 
 const game = new Phaser.Game(config);
+
+// фикс для Safari, Telegram WebView и Android браузеров
+function fixViewportHeight() {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+window.addEventListener('load', fixViewportHeight);
+window.addEventListener('resize', fixViewportHeight);
+window.addEventListener('orientationchange', () => {
+  setTimeout(fixViewportHeight, 300);
+});
