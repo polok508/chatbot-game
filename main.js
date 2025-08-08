27668439570f85267cccb38c09f3b4581aeabf4c
polok508 <polok508@gmail.com>
2025-08-08 14:27:16 +0300

    Фикс 3

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
 window.addEventListener('load', () => {
   const game = new Phaser.Game(config);
-
-  function resizeGame() {
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-    game.scale.resize(width, height);
-  }
-
-  window.addEventListener('resize', resizeGame);
-  window.addEventListener('orientationchange', () => {
-    setTimeout(resizeGame, 300); 
-  });
 });
