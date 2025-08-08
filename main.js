5c58db074ac27d0303ac02b83bcdfb193f05ac59
polok508 <polok508@gmail.com>
2025-08-09 00:08:50 +0300

    ...

--- main.js
+++ main.js
@@ -53,14 +53,14 @@ function checkOrientation() {
         rotatedToLandscape = true;
         showElement('scroll-notice');
         body.style.overflowY = 'auto';
-        body.style.height = '150vh';
+        body.style.height = '150vh'; // уменьшили scroll area
         return;
       }
     } else {
       if (!gameStarted) {
         showElement('scroll-notice');
         body.style.overflowY = 'auto';
-        body.style.height = '150vh'; 
+        body.style.height = '150vh'; // уменьшили scroll area
       } else {
         showElement('game-container');
         body.style.overflowY = 'hidden';
